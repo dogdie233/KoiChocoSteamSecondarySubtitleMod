2dd8350f86596c33ceeb903bd324dbeaea699347
dogdie233 <dogdie233@163.com>
2025-03-30 04:13:46 +0800

    fix: 在选择页面进入Backlog再退出Backlog会导致第二语言的字幕重复生成

--- KoiChocoSteamSecondarySubtitle/Patches/AdvChoicePatch.cs
+++ KoiChocoSteamSecondarySubtitle/Patches/AdvChoicePatch.cs
@@ -24,14 +24,23 @@ public class AdvChoicePatch
     
     [HarmonyPatch(typeof(AdvChoice), nameof(AdvChoice.ShowMenu))]
     [HarmonyPrefix]
-    public static void ShowMenuPostfix(List<string>[] mplist)
+    public static void ShowMenuPrefix(List<string>[] mplist, ref List<string> __state)
     {
         if (mplist.Length <= 1 || SecondaryLanguage.SecLang == EngineMain.Language.none) return;
         var primaryLangChoices = mplist[(int)EngineMain.lang];
         var secondaryLangChoices = mplist[(int)SecondaryLanguage.SecLang];
         var newList = new List<string>(primaryLangChoices);  // 复制一个新的列表
-        for (var i = 0; i < newList.Count && i < secondaryLangChoices.Count; i++)
-            newList[i] = $"<size=18>{newList[i]}</size>\n<size=14>{secondaryLangChoices[i]}</size>";
+        for (var i = 0; i < newList.Count && i < primaryLangChoices.Count; i++)
+            newList[i] = $"<size=18>{primaryLangChoices[i]}</size>\n<size=14>{secondaryLangChoices[i]}</size>";
+        __state = primaryLangChoices;
         mplist[(int)EngineMain.lang] = newList;
+    }
+    
+    [HarmonyPatch(typeof(AdvChoice), nameof(AdvChoice.ShowMenu))]
+    [HarmonyPostfix]
+    public static void ShowMenuPostfix(List<string>[] mplist, ref List<string> __state)
+    {
+        if (__state == null || mplist.Length <= 1 || SecondaryLanguage.SecLang == EngineMain.Language.none) return;
+        mplist[(int)EngineMain.lang] = __state;
     }
 }
